ae9881bd76383d69f0eea8dcca9f6b690017cf2d
Harsh Rachalwar <f20210200@hyderabad.bits-pilani.ac.in>
2024-03-03 01:34:04 +0530

    new

--- main.py
+++ main.py
@@ -393,7 +393,7 @@ if __name__ == '__main__':
 
             unlab_features = features[-1]
     
-    tsne = TSNE(n_components=2,random_state=42)
+    tsne = TSNE(n_components=2,random_state=42,perplexity=50)
 
     train_tsne = tsne.fit_transform(lab_features)
     test_tsne = tsne.fit_transform(unlab_features)
