cbcf8aa59bab03317916bf3cf0e196613551fd7f
Harsh Rachalwar <f20210200@hyderabad.bits-pilani.ac.in>
2024-03-03 11:53:20 +0530

    trial

--- main.py
+++ main.py
@@ -431,19 +431,20 @@ if __name__ == '__main__':
     ner_unlabpred = ner_unlabpred.cpu()
     re_unlabpred = re_unlabpred.cpu()
     # Step 5: Visualize t-SNE plots
+    print("done tsne train")
     plt.figure(figsize=(10, 5))
 
     # Train t-SNE plot
     plt.subplot(1, 2, 1)
-    plt.scatter(train_tsne[:, 0], train_tsne[:, 1], c='blue')#, cmap='viridis')
+    plt.scatter(train_tsne[:, 0], train_tsne[:, 1], c=ner_labpred+re_labpred, cmap='viridis')
     plt.title('Train t-SNE Plot')
 
     # Test t-SNE plot
     plt.subplot(1, 2, 2)
-    plt.scatter(test_tsne[:, 0], test_tsne[:, 1], c='red')#, cmap='viridis')
+    plt.scatter(test_tsne[:, 0], test_tsne[:, 1], c=ner_unlabpred+re_unlabpred, cmap='viridis')
     plt.title('Test t-SNE Plot')
 
     plt.tight_layout()
     plt.show()
 
-    print("done")
+    print("done plotting")
